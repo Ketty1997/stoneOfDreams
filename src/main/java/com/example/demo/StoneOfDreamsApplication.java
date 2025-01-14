aca8f4033f32fb5e3a4157b7c12a5e5e6943b6b1
katia <katiafalletti97@gmail.com>
2025-01-14 16:30:57 +0100

    first commit

---
+++ src/main/java/com/example/demo/StoneOfDreamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoneOfDreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoneOfDreamsApplication.class, args);
+	}
+
+}
